c26cdf58c02145036c2927ec19f09a874f47c3e9
Alvin <alvinoforiadjei@gmail.com>
2018-08-24 17:19:16 +0300

    s

--- MyFriendsRoom/ViewController.swift
+++ MyFriendsRoom/ViewController.swift
@@ -22,8 +22,7 @@ class ViewController: UIViewController {
         //asd
         //null
         //hfghj
-        
-        //testing 
+        //testing  
     }
 
     override func didReceiveMemoryWarning() {
