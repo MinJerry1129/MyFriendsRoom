99de3b878b5f4282e93bfb68d3ca85ab3e38a3b0
Alvin <alvinoforiadjei@gmail.com>
2018-08-24 17:53:30 +0300

    jhkuhhk

--- MyFriendsRoom/ViewController.swift
+++ MyFriendsRoom/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         //asd
         //null
         //hfghj
+        
+        //=================================
         //testing
         //lkhdfkjhdkghkdhfkdkfhkjgdfhk
         //dskfhkjdhfkgjdf
