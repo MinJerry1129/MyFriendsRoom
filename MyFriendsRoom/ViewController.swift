ec7e94b3fa4ec28e3012646b6dd63052da971bb1
Alvin <alvinoforiadjei@gmail.com>
2018-08-24 15:06:18 +0300

    Hello world

--- MyFriendsRoom/ViewController.swift
+++ MyFriendsRoom/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Hello World 
     }
 
     override func didReceiveMemoryWarning() {
